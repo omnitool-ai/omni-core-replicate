ad8698e7c7d0d65ad762cc48fd3436e1260b158a
Gezo <georg@gulbsoft.org>
2023-10-18 23:53:05 +0800

    fix

--- server/extension.js
+++ server/extension.js
@@ -1,97 +1,87 @@
+// Import necessary dependencies
+import resolveScript from './resolve.js';
 
+/**
+ * Debug function for development
+ * @param {string} text - The text to debug
+ */
+const debug = (text) => {
+  // Uncomment this line to log text during debugging
+  // console.log(text);
+};
 
-const debug = (text)=>
-{
-  //console.log(text)
-}
-
-const extensionHooks = {
-
-
-  'pre_request_execute': function(ctx, ...args)
-  {
-    debug('pre_request_execute')
-
-  }
-  ,
-  'post_request_execute': function(ctx, ...args)
-  {
-    debug('post_request_execute')
-  }
-  ,
-  'job_started': function(ctx, ...args)
-  {
-    debug('job_started')
-  }
-  ,
-  'job_finished': function(ctx, ...args)
-  {
-    debug('job_finished')
-  }
-
-}
-
-import resolveScript from '../scripts/server/resolve.js'
-
-
+/**
+ * Extension methods object
+ */
 const extensionMethods = {
+  /**
+   * Resolves a missing block given a context and blockKey
+   * @param {Object} ctx - The context for the operation
+   * @param {string} blockKey - The block key
+   * @return {Object|undefined} - Resolved block or undefined
+   */
   'resolveMissingBlock': async (ctx, blockKey) => {
-      //strip 'run.' prefix from blockKey
-      blockKey = blockKey.replace('run.', '')
-      const result = await resolveScript.exec(ctx, [blockKey])
-      return result?.block || undefined
+    // Remove 'run.' prefix from blockKey
+    blockKey = blockKey.replace('run.', '');
+    const result = await resolveScript.exec(ctx, [blockKey]);
+    return result?.block || undefined;
   }
- }
+};
 
-
-
-
+/**
+ * Macros object
+ */
 let macros = {
-
-"omni-core-replicate:replicate_exec": async (payload, ctx, component) => {
-  if (payload.enabled === false)
-  {
-    return {}
-  }
-  for (let key in payload) {
-    if (payload[key] === '' || payload[key] === undefined) {
-      delete payload[key];
+  /**
+   * Execute  logic for Replicate.com components
+   * @param {Object} payload - The payload for the operation
+   * @param {Object} ctx - The context for the operation
+   * @param {Object} component - The component being operated on
+   * @return {Object} - The result of the operation
+   */
+  "omni-core-replicate:replicate_exec": async (payload, ctx, component) => {
+    if (payload.enabled === false) {
+      return {};
     }
 
-    // This is needed for now as the select control does not handle numbers correctly
-    if (component.inputs[key].type === 'integer')
-    {
-      payload[key] = parseInt(payload[key]);
+    for (let key in payload) {
+      // Remove undefined or empty string keys from the payload
+      if (payload[key] === '' || payload[key] === undefined) {
+        delete payload[key];
+      }
+
+      // Handle input type conversions
+      if (component.inputs[key].type === 'integer') {
+        payload[key] = parseInt(payload[key]);
+      } else if (['number', 'float'].includes(component.inputs[key].type)) {
+        payload[key] = parseFloat(payload[key]);
+      }
     }
-    else if (['number','float'].includes(component.inputs[key].type))
-    {
-      payload[key] = parseFloat(payload[key]);
-    }
+
+    // Deep clone the replicate property from component.custom to payload._replicate
+    payload._replicate = JSON.parse(JSON.stringify(component.custom.replicate));
+
+    return await ctx.app.api2.execute(
+      `${component.name}`,
+      payload,
+      { responseContentType: component.data.responseContentType },
+      { user: ctx.userId }
+    );
   }
-  payload._replicate = JSON.parse(
-    JSON.stringify(component.custom.replicate)
-  );
-  return await ctx.app.api2.execute(
-    `${component.name}`,
-    payload,
-    { responseContentType: component.data.responseContentType },
-    { user: ctx.userId }
-  );
-  }
+};
 
-}
-
-
-const createComponents =  () => ({
+/**
+ * Factory function to create components
+ * @return {Object} - Components object
+ */
+const createComponents = () => ({
   blocks: [],
   patches: [],
   macros: macros,
+});
 
-})
-
-export default
-{
-  extensionHooks,
+// Default export object
+export default {
   extensionMethods,
   createComponents
-}
+};
